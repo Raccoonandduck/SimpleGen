5edff4403084cf38700f680729c2cb5f2d4e6633
Raccoonandduck <Raccoonandduck@gmail.com>
2013-09-08 19:48:08 +0700

    Добавил index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'function.php'; 
+head();
+?>
+<style>
+a:nth-of-type(1) li{
+color:white;
+background:black;
+}
+</style>
+<h2><a href='countdown.php'>1. Таймер обратного отсчет</a></h2>
+<h2><a href='popup.php'>2. Всплывающее окно</a></h2>
+<h2><a href='slider.php'>3. Слайдер</a></h2>
+<h2><a href='calculation.php'>4. Калькулятор</a></h2>
+
+<?php include 'footer.html' ?>
