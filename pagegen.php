2d4264309639db68b431f6a5f463ddaed70c038f
Zubrolet <famzu@mail.ru>
2013-09-11 23:27:28 +0700

    2 новых <div> внутри одного

--- pagegen.php
+++ pagegen.php
@@ -5,11 +5,19 @@ head();
 
 <h2>Конструктор страниц</h2>
 
-<div id="mainPageGen" width=100%><div>
+<div id="mainPageGen" width=100%>
+    <div id="leftPageGen"></div>
+    <div id="rightPageGen"></div>
+</div>
+
 
 <script>
-var mainJS =document.getElementById('mainPageGen');
+var mainJS = document.getElementById('mainPageGen');
+var leftJS = document.getElementById('leftPageGen');
+var rightJS = document.getElementById('rightPageGen');
 mainJS.style.height = window.innerHeight+'px';
+leftJS.style.height = window.innerHeight+'px';
+rightJS.style.height = window.innerHeight+'px';
 </script>
 
 <script>
