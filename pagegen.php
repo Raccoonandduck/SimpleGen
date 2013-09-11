39c7c43c3b0bc4fd05709a21de8c67c0304ec89e
Zubrolet <famzu@mail.ru>
2013-09-11 22:01:08 +0700

    Рабочая область представляет один div

--- pagegen.php
+++ pagegen.php
@@ -2,10 +2,19 @@
 include 'function.php';
 head();
 ?>
+
 <h2>Конструктор страниц</h2>
+
+<div id="mainPageGen" width=100%><div>
 <script>
-alert("Сервер временно не работает");
+var mainJS =document.getElementById('mainPageGen');
+mainJS.style.height = window.innerHeight+'px';
 </script>
+
+<script>
+alert("Сервис временно не работает");
+</script>
+
 <?php
 footer();
 ?>
